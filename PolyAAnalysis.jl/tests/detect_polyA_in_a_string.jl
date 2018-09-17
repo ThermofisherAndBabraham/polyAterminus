10e3cff8e0bb676eac8ada268539a757ba500b05
Gytis <gytis.mackevicius@thermofisher.com>
2018-09-17 16:18:07 +0300

    Updated <*tests>: Tests.

--- PolyAAnalysis.jl/tests/detect_polyA_in_a_string.jl
+++ PolyAAnalysis.jl/tests/detect_polyA_in_a_string.jl
@@ -8,66 +8,78 @@
 ##    )::Bool
 ##
 
+minimum_polyA_length=10
+min_l = Int64(minimum_polyA_length / 2)
+re = Regex("(A+[GTC])?(A{$min_l,})([GTC]A+)?")
+
 seq = "AGAGAATTACAAATCAGAAGGGGAAGATTGACTGTTTAATAAAATGTGCTGGGAATCAATGGCAAATATACAATAAAATAAAATTATATTTTTATTCATATTATACCCCAAATAAATTTCAGATATACATAAAACCCAAAGTAAAATATT"
-@test !detect_polyA_in_a_string(seq,10,1,debug=true)
+@test !detect_polyA_in_a_string(seq,10,1,re,debug=true)
 
 seq = "GAATGTATGGTAGGAATGTATTCTCTTGTAGGAATGTAAATCTGTATTAAAAGGGGGTCCAAGCCAGGCCCCCAGGTCTTCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAA"
-@test detect_polyA_in_a_string(seq,10,1,debug=true)
+@test detect_polyA_in_a_string(seq,10,1,re,debug=true)
 
 seq = "AGAGAATTACAAATCAGAAGGGGAAGATTGACTGTTTAATAAAATGTGCTGGGAATCAATGGCAAATATACAATAAAATAAAATTATATTTTTATTCATATTATACCCCAAATAAATTTCAGATATACATAAAACCCAAAGTAAAATATT"
-@test detect_polyA_in_a_string(seq,10,3,debug=true)
+@test detect_polyA_in_a_string(seq,10,3,re,debug=true)
 
 seq = "AGAGAATTACAAATCAGAAGGGGAAGATTGACTGTTTAATAAAATGTGCTGGGAATCAATGGCAAATATACAATAAAATAAAATTATATTTTTATTCATATTATACCCCAAATAAATTTCAGATATACATAAAACCCAAAGTAAAATATT"
-@test !detect_polyA_in_a_string(seq,10,1,debug=true)
+@test !detect_polyA_in_a_string(seq,10,1,re,debug=true)
 
 seq = "TCAAAAAAATAATATGGTAATAATAATAAAAGCAGTGCCCAGAGAACAAGGCTTGTTGGCTGTTCCACCCCAGGGGGCCCCTTGCACAGGCGGTGCCATCTCTGCCTCCCAAAGCTCTAAGAGCCACTGTCCCCCATCCCAAGAGA"
-@test !detect_polyA_in_a_string(seq,10,1,debug=true)
+@test !detect_polyA_in_a_string(seq,10,1,re,debug=true)
 
 seq = "TCAAAAAAATAATATGGTAATAATAATAAAAGCAGTGCCCAGAGAACAAGGCTTGTTGGCTGTTCCACCCCAGGGGGCCCCTTGCACAGGCGGTGCCATCTCTGCCTCCCAAAGCTCTAAGAGCCACTGTCCCCCATCCCAAGAGA"
-@test !detect_polyA_in_a_string(seq,10,1,debug=true)
+@test !detect_polyA_in_a_string(seq,10,1,re,debug=true)
 
 seq = "GGGGCGCGGCGGCGCGGGGGGGGAAAAAAAAACCCCCCCCCCCCCCCGGGGGGCCCC"
-@test !detect_polyA_in_a_string(seq,10,1,debug=true)
+@test !detect_polyA_in_a_string(seq,10,1,re,debug=true)
 
 seq = "GGGGCGCGGCGGCGCGGGGGGGGAAAATAAAAACCCCCCCCCCCCCCCGGGGGGCCCC"
-@test detect_polyA_in_a_string(seq,10,1,debug=true)
+@test detect_polyA_in_a_string(seq,10,1,re,debug=true)
+
+seq = "AAGAAAAAAACAAACAACCCCATCAAAAAGTGGGCAAAGGATATAAATAGACAC"
+@test detect_polyA_in_a_string(seq,10,1,re,debug=true)
+
+seq = "TCAGGAGATCGAGACCATCCTGGCTAACATGGTGAAACCCCGTCTCTACTAAAAATAACAAAAAAAAA"
+@test detect_polyA_in_a_string(seq,10,1,re,debug=true)
 
 seq = "TGTGTGTGTGTGTGGTAAAAAAAAAAGT"
-@test detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "TGTGTGTGTGTGTGGTAAAAAAAAAA"
-@test detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "TGTGTGTGTGTGTGGTAAAAAAAAATAGGGG"
-@test detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "TGTGTGTGTGTGTGGATAAAAAAAAA"
-@test detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "TGTGTGTGTGTGTGGAATAAAAAAAAA"
-@test detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "TGTGTGTGTGTGTGGAAAAAAAAATAA"
-@test detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "TGTGTGTGTGTGTGGAATAAAAAAAAATAA"
-@test detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
+
+seq = "AAAAAAAAAAAAAAAAAAAATGGAACGCAGGGCAGGAACTCGTATTTGGGGGGAGATGGG"
+@test detect_polyA_in_a_string(seq,10,1,re,debug=true)
 
 seq = "TGTGTGTGTGTGTGGAAAAAAAAAT"
-@test !detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test !detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "TGTGTGTGTGTGTGGTAAAAAAAAA"
-@test !detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test !detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "TGTGTGTGTGTGTGGAAAAAAAAA"
-@test !detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test !detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "AAAAAAAAAAAAAAATGTGTGTGTGTGTGGAAAAAAAAA"
-@test !detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test !detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "AAAAAAAAAAAAAAA"
-@test detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
+@test detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
 
 seq = "AAAAAAAAA"
-@test !detect_polyA_in_a_string(seq,10,1,maximum_search_fragment_length=20)
-#GAATGTATGGTAGGAATGTATTCTCTTGTAGGAATGTAAATCTGTATTAAAAGGGGGTCCAAGCCAGGCCCCCAGGTCTTCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAA
+@test !detect_polyA_in_a_string(seq,10,1,re,maximum_search_fragment_length=20)
--- PolyAAnalysis.jl/tests/get_polyA_prefixes.jl
+++ PolyAAnalysis.jl/tests/get_polyA_prefixes.jl
@@ -4,45 +4,48 @@
 ##    minimum_not_polyA::Int64,
 ##    minimum_polyA_length::Int64)::Array{String,1}
 ##
+minimum_not_polyA = 20
+minimum_polyA_length = 10
+re = Regex("([ATGC]{$minimum_not_polyA})A{$minimum_polyA_length,}")
 
 # Test set 1 just a simple nt real example
 seq = dna"ATCCTTGGCTTTCTTCCGGGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 output = ["ATCCTTGGCTTTCTTCCGGG","CAAAAAAAAAAAAAAAAAAA", "GGGGGGGGGGGGGGGGGGGG"]
 ref_seq=FASTA.Record("seq",seq)
-@test get_polyA_prefixes(ref_seq,20,10)  == output
+@test get_polyA_prefixes(ref_seq,20,10,re)  == output
 
 # Test set 2 no AAAA
 seq = dna"ATCCTTGGCTTTCTTCCGGGCGGGGGGGGGGGGGGGGGGGGAAA"
 output = []
 ref_seq=FASTA.Record("seq",seq)
-@test get_polyA_prefixes(ref_seq,20,10)  == output
+@test get_polyA_prefixes(ref_seq,20,10,re)  == output
 
 # Test set 3
 seq = dna"ATCCTTGGCTTTCTTCCGGGCGGGGGGGGGGGGGGGGAGAGAGAAAAAAAAAGAAAAAAAAAA"
 output = ["GGGGAGAGAGAAAAAAAAAG"]
 ref_seq=FASTA.Record("seq",seq)
-@test get_polyA_prefixes(ref_seq,20,10)  == output
+@test get_polyA_prefixes(ref_seq,20,10,re)  == output
 
 # Test set 4 Only AA...
 seq = dna"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 output = ["AAAAAAAAAAAAAAAAAAAA"]
 ref_seq=FASTA.Record("seq",seq)
-@test get_polyA_prefixes(ref_seq,20,10)  == output
+@test get_polyA_prefixes(ref_seq,20,10,re)  == output
 
 # Test set 5 Only AA... but too short
 seq = dna"AAAAAAAAAAA"
 output = []
 ref_seq=FASTA.Record("seq",seq)
-@test get_polyA_prefixes(ref_seq,20,10)  == output
+@test get_polyA_prefixes(ref_seq,20,10,re)  == output
 
 # Test set 6
 seq = dna"AAAAAAAAAAGGGGGGGGGGAAAAAAAAAA"
 output = ["AAAAAAAAAAGGGGGGGGGG"]
 ref_seq=FASTA.Record("seq",seq)
-@test get_polyA_prefixes(ref_seq,20,10)  == output
+@test get_polyA_prefixes(ref_seq,20,10,re)  == output
 
 # Test set 7 too short
 seq = dna"GTCGTCGTC"
 output = []
 ref_seq=FASTA.Record("seq",seq)
-@test get_polyA_prefixes(ref_seq,20,10)  == output
+@test get_polyA_prefixes(ref_seq,20,10,re)  == output
